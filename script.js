3ecb4c547b018299e60d3a4773f16ac62845765c
Julia Khayrulina <julia748@mail.ru>
2021-07-17 13:36:24 +0300

    added: the ability to destroy ships and keep values in the record

---
+++ script.js
@@ -0,0 +1,113 @@
+const record = document.querySelector('#record');
+const shot = document.querySelector('#shot');
+const hit = document.querySelector('#hit');
+const dead = document.querySelector('#dead');
+const enemy = document.querySelector('#enemy');
+const againBtn = document.querySelector('#again');
+const header = document.querySelector('.header');
+
+const game = {
+  ships: [
+    {
+      location: ['26', '36', '46', '56'],
+      hit: ['', '', '', ''],
+    },
+    {
+      location: ['11', '12', '13'],
+      hit: ['', '', ''],
+    },
+    {
+      location: ['69', '79'],
+      hit: ['', ''],
+    },
+    {
+      location: ['32'],
+      hit: [''],
+    },
+  ],
+  shipCount: 4,
+};
+const play = {
+  record: localStorage.getItem('battleShipRecord') || 0,
+  shot: 0,
+  hit: 0,
+  dead: 0,
+  set updateData(data) {
+    if (game.shipCount < 1) {
+      return;
+    } else {
+      this[data] += 1;
+      this.render();
+    }
+  },
+  render() {
+    record.textContent = this.record;
+    shot.textContent = this.shot;
+    hit.textContent = this.hit;
+    dead.textContent = this.dead;
+  },
+};
+
+const show = {
+  hit(elem) {
+    this.changeClass(elem, 'hit');
+  },
+  miss(elem) {
+    if (game.shipCount < 1) return;
+    else this.changeClass(elem, 'miss');
+  },
+  dead(elem) {
+    this.changeClass(elem, 'dead');
+  },
+  changeClass(elem, value) {
+    elem.className = value;
+  },
+};
+
+const fire = (event) => {
+  const target = event.target;
+  if (target.matches('.miss' || target.tagname !== 'TD')) return;
+  show.miss(target);
+  play.updateData = 'shot';
+
+  for (let i = 0; i < game.ships.length; i++) {
+    const ship = game.ships[i];
+    const index = ship.location.indexOf(target.id);
+
+    if (index >= 0) {
+      show.hit(target);
+      play.updateData = 'hit';
+      ship.hit[index] = 'x';
+      const life = ship.hit.indexOf('');
+      if (life < 0) {
+        play.updateData = 'dead';
+        for (const id of ship.location) {
+          show.dead(document.getElementById(id));
+        }
+
+        game.shipCount -= 1;
+        if (game.shipCount < 1) {
+          header.textContent = 'Игра окончена!';
+          header.style.color = 'green';
+
+          if (play.shot < play.record || play.record === 0) {
+            localStorage.setItem('battleShipRecord', play.shot);
+            play.record = play.shot;
+            play.render();
+          }
+        }
+      }
+    }
+  }
+};
+
+const initGame = () => {
+  enemy.addEventListener('click', fire);
+  play.render();
+
+  againBtn.addEventListener('click', () => {
+    location.reload();
+  });
+};
+
+initGame();
